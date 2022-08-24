c1341a55e918731e832cbc444b8799d57094adbf
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-24 15:46:21 -0700

    function works
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -17,69 +17,30 @@ function diceRoll() {
 
 
 
-
-function runTurn(player) {
-  player.turnScore = diceRoll()
-  if (player.turnScore === 0) {
-    console.log("you rolled a zero")
-    return
-  } else { 
-    player.currentScore += player.turnScore
-    //add the 2 event listeners
-    //add functions to handle them?
-    // the hold function will add turnscore to currentscore
-    // the keepgoing function will roll again and add to turnscore and reask the player if they wanna keep going
-  }
-}
-
-
-function turnOrder(player1, player2) {
-  let breaknum = 0
-  let turnNumber = 0
-  while (true) {
-    turnNumber += 1
-    if (turnNumber %2 === 1) {
-      console.log("player 1's turn")
-      runTurn(player1)
-      scoreLimit(player1)
-    } else {
-      console.log("player 2's turn")
-      runTurn(player2)
-      scoreLimit(player2)
-    }
-    console.log(player1.currentScore)
-    console.log(player2.currentScore)
-    //player2.currentScore += 10
-    breaknum += 1
-    if (player1.loopBreak === 1 || player2.loopBreak === 1) {
-      break;
-    }
-    //if (breaknum === 30) {
-      //console.log("we hit the breaknum")
-      //break;
-    //}
-  }
-}
-
-
-
-function startTurn() {
-  console.log("turn started")
+function startTurn(player, turnNumber) {
+  //console.log(call(player1))
+  let thing = document.getElementById("player1Info").innerText
   document.getElementById("start").setAttribute("class", "hidden");
-  document.getElementById("kG1").removeAttribute("class");
-  document.getElementById("eT1").removeAttribute("class");
+  let roll = diceRoll()
+    if (roll === 0) {
+      //sorry you rolled a 1
+      console.log("rolled a 1")
+      document.getElementById("eT1").removeAttribute("class");
+    } else {
+      //we need to output the number rolled
+      console.log(roll)
+      console.log("its turn number: " + turnNumber)
+      console.log("turn started")
+      console.log(player)
+      document.getElementById("kG1").removeAttribute("class");
+      document.getElementById("eT1").removeAttribute("class");
+    }
+  
+  turnNumber += 1
+  return [turnNumber, roll]
 }
 
-function keepGoing() {
-  console.log("Keep GOING!")
-}
-function endTurn() {
-  console.log("hold it right there!")
-  document.getElementById("start").removeAttribute("class");
-  document.getElementById("kG1").setAttribute("class", "hidden");
-  document.getElementById("eT1").setAttribute("class", "hidden");
-  
-}
+
 
 
 
@@ -115,21 +76,62 @@ function beginGame(event) {
   console.log("we pushed the button")
   let player1 = new Player(0, 0, 1, 0)
   let player2 = new Player(0, 0, 2, 0)
-  document.querySelector("button.keepGoing").addEventListener("click", keepGoing);
-  document.querySelector("button.endTurn").addEventListener("click", endTurn);
-  document.querySelector("button.startTurn").addEventListener("click", startTurn);
+  let turnNumber = 1
 
+  document.querySelector("button.keepGoing").addEventListener("click", function (event) {
+    event.preventDefault();
+    let roll = diceRoll()
+    if (turnNumber %2 === 0) {
+      if (roll === 0) {
+        player1.turnScore = 0
+        document.getElementById("kG1").setAttribute("class", "hidden");
+      } else (player1.turnScore += roll)
+    }
+    if (turnNumber %2 === 1) {
+      if (roll === 0) {
+        player2.turnScore = 0
+        document.getElementById("kG1").setAttribute("class", "hidden");
+      } else (player2.turnScore += roll)
+    }
+    
+  });
+
+
+
+  document.querySelector("button.endTurn").addEventListener("click", function (event) {
+    event.preventDefault()
+    player1.currentScore += player1.turnScore;
+    player2.currentScore += player2.turnScore;
+    player1.turnScore = 0
+    player2.turnScore = 0
+    console.log("hold it right there!")
+    document.getElementById("start").removeAttribute("class");
+    document.getElementById("kG1").setAttribute("class", "hidden");
+    document.getElementById("eT1").setAttribute("class", "hidden");
+    scoreLimit(player1)
+    scoreLimit(player2)
+  });
+
+  document.querySelector("button.startTurn").addEventListener("click", function(event) {
+    event.preventDefault()
+    if (turnNumber %2 === 1) {
+      let startOut = startTurn(player1, turnNumber)
+      console.log(startOut)
+      turnNumber = startOut[0]
+      player1.turnScore = startOut[1]
+      console.log(player1)
+
+    } else {
+      let startOut = startTurn(player2, turnNumber)
+      console.log(startOut)
+      turnNumber = startOut[0]
+      player2.turnScore = startOut[1]
+      console.log(player2)
+    }
+  })
 }
 
 window.addEventListener("load", function (){
   document.querySelector("form#newGame").addEventListener("submit", beginGame); 
   
 });
-
-//document.querySelector("button.dele").addEventListener("click", handleDelete);
-
-
-/* html needs: a way to display dice rolls, and buttons for user actions
-
-js needs: to run each turn and have listeners for those buttons
-*/
